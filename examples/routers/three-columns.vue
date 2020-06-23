75daa31dc0e22dfdf8ec96c4c937984623698c7f
SorrowX <1356988206@qq.com>
2020-06-24 00:08:59 +0800

    3列布局

--- examples/routers/three-columns.vue
+++ examples/routers/three-columns.vue
@@ -1,25 +1,90 @@
 <template>
     <div class="test-three-columns">
-        <lay-three-columns>
+        <!-- <lay-three-columns adapt="right" leftWidth="100px" centerWidth="200px">
             <template v-slot:left>
-                left
+                <fake-content v-for="num in 5" :key="num">{{ num }}</fake-content>
             </template>
             <template v-slot:center>
-                center
+                <fake-content v-for="num in 7" :key="num">{{ num }}</fake-content>
             </template>
             <template v-slot:right>
-                right
+                <fake-content v-for="num in 9" :key="num">{{ num }}</fake-content>
+            </template>
+        </lay-three-columns> -->
+
+        <!-- <lay-three-columns adapt="right" leftWidth="100px" centerWidth="200px" height="500px">
+            <template v-slot:left>
+                <fake-content v-for="num in 5" :key="num">{{ num }}</fake-content>
+            </template>
+            <template v-slot:center>
+                <fake-content v-for="num in 7" :key="num">{{ num }}</fake-content>
+            </template>
+            <template v-slot:right>
+                <fake-content v-for="num in 9" :key="num">{{ num }}</fake-content>
+            </template>
+        </lay-three-columns> -->
+
+        <!-- <lay-three-columns adapt="right" leftWidth="100px" centerWidth="200px" height="500px" nativeScrollbar>
+            <template v-slot:left>
+                <fake-content v-for="num in 5" :key="num">{{ num }}</fake-content>
+            </template>
+            <template v-slot:center>
+                <fake-content v-for="num in 7" :key="num">{{ num }}</fake-content>
+            </template>
+            <template v-slot:right>
+                <fake-content v-for="num in 9" :key="num">{{ num }}</fake-content>
+            </template>
+        </lay-three-columns> -->
+
+        <!-- <lay-three-columns adapt="right" leftWidth="100px" centerWidth="200px" height="500px" overflow="hidden">
+            <template v-slot:left>
+                <fake-content v-for="num in 5" :key="num">{{ num }}</fake-content>
+            </template>
+            <template v-slot:center>
+                <fake-content v-for="num in 7" :key="num">{{ num }}</fake-content>
+            </template>
+            <template v-slot:right>
+                <fake-content v-for="num in 9" :key="num">{{ num }}</fake-content>
+            </template>
+        </lay-three-columns> -->
+
+        <lay-three-columns 
+            adapt="right" 
+            leftWidth="100px" 
+            centerWidth="200px" 
+            height="500px" 
+            overflow="hidden"
+            :leftStyle="[{ border: '1px solid red' }, { color: 'red' }]"
+            :centerClass="centerClass"
+        >
+            <template v-slot:left>
+                <fake-content v-for="num in 5" :key="num">{{ num }}</fake-content>
+            </template>
+            <template v-slot:center>
+                <fake-content v-for="num in 7" :key="num">{{ num }}</fake-content>
+            </template>
+            <template v-slot:right>
+                <fake-content v-for="num in 9" :key="num">{{ num }}</fake-content>
             </template>
         </lay-three-columns>
     </div>
 </template>
 
 <script>
+    import Prism from 'prismjs';
+    import FakeContent from '../components/fake-content'
     export default {
-        name: ''
+        components: { FakeContent },
+        data() {
+            return {
+                centerClass: { c1: true, c2: true }
+            }
+        }
     }
 </script>
 
 <style lang="scss">
-    
+    .c1 {
+        border: 1px solid green;
+    }
 </style>
--- src/components/three-columns/right-adapt.vue
+++ src/components/three-columns/right-adapt.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="lay-right-adapt">
-        <div class="lay-right-adapt__left">
-            <slot name="left"></slot>
+        <div class="lay-right-adapt__left" :class="leftClass" :style="calcLeftStyle">
+            <lay-scrollbar :native="native">
+                <slot name="left"></slot>
+            </lay-scrollbar>
         </div>
-        <div class="lay-right-adapt__center">
-            <slot name="center"></slot>
+        <div class="lay-right-adapt__center" :class="centerClass" :style="calcCenterStyle">
+            <lay-scrollbar :native="native">
+                <slot name="center"></slot>
+            </lay-scrollbar>
         </div>
-        <div class="lay-right-adapt__right">
-            <slot name="right"></slot>
+        <div class="lay-right-adapt__right" :class="rightClass" :style="calcRightStyle">
+            <lay-scrollbar :native="native">
+                <slot name="right"></slot>
+            </lay-scrollbar>
         </div>
     </div>
 </template>
@@ -15,12 +21,53 @@
 <script>
     import threeColumnsMixin from '@/mixins/three-columns-mixin'
     import LayScrollbar from '@/components/scrollbar'
+    import { isArray, isObject } from '@/utils/utils'
     export default {
-        name: '',
+        name: 'RightAdapt',
         mixins: [ threeColumnsMixin ],
         components: { LayScrollbar },
         computed: {
-            
+            native() {
+                if (
+                    this.overflow === 'hidden' ||
+                    (this.height === 'auto' && this.overflow === 'auto')
+                ) {
+                    return true
+                }
+                return this.nativeScrollbar
+            },
+            calcLeftStyle() {
+                return this.getStyle({
+                    width: this.leftWidth,
+                    height: this.height,
+                    overflow: this.overflow
+                }, this.leftStyle)
+            },
+            calcCenterStyle() {
+                return this.getStyle({
+                    width: this.centerWidth,
+                    height: this.height,
+                    overflow: this.overflow
+                }, this.centerStyle)
+            },
+            calcRightStyle() {
+                return this.getStyle({
+                    height: this.height,
+                    overflow: this.overflow
+                }, this.rightStyle)
+            }
+        },
+        methods: {
+            getStyle(style, propStyle) {
+                let arrStyle = []
+                arrStyle.push(style)
+                if (isObject(propStyle)) {
+                    arrStyle.push(propStyle)
+                } else if (isArray(propStyle)) {
+                    arrStyle = arrStyle.concat(propStyle)
+                }
+                return arrStyle
+            }
         }
     }
 </script>
